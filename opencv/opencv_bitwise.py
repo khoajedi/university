eca7d41da26dd7e168fd76a890bef032cc5b24c1
Khoa Vo <khoa.jedi@gmail.com>
2021-12-01 23:34:29 +0700

    Add opencv bitwise

---
+++ opencv/opencv_bitwise.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+rectangle = np.zeros((300, 300), dtype="uint8")
+cv2.rectangle(rectangle, (25, 25), (275, 275), 255, -1)
+cv2.imshow("Rectangle", rectangle)
+
+circle = np.zeros((300, 300), dtype="uint8")
+cv2.circle(circle, (150, 150), 150, 255, -1)
+cv2.imshow("Circle", circle)
+
+cv2.waitKey(0)
+
+bitwiseAnd = np.bitwise_and(rectangle, circle)
+cv2.imshow("AND", bitwiseAnd)
+
+bitwiseOr = np.bitwise_or(rectangle, circle)
+cv2.imshow("OR", bitwiseOr)
+
+bitwiseXor = np.bitwise_xor(rectangle, circle)
+cv2.imshow("XOR", bitwiseXor)
+
+bitwiseNot = np.bitwise_not(rectangle)
+cv2.imshow("NOT", bitwiseNot)
+
+cv2.waitKey(0)
