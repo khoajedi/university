84000e556ad71b63cfc36e5577d0bf3a8cc70807
Khoa D. Vo <khoa.jedi@gmail.com>
2021-12-25 08:08:38 +0700

    Add houses model

---
+++ deep_learning/jedi/houses_model.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+
+def create_mlp(dim, regress=False):
+    model = Sequential()
+    model.add(Dense(8, input_dim=dim, activation="relu"))
+    model.add(Dense(4, activation="relu"))
+
+    if regress:
+        model.add(Dense(1, activation="linear"))
+
+    return model
