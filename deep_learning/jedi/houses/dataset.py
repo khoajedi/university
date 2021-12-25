e7f98fba646f52def5b782b0a41ad77e0a7e2e6e
Khoa D. Vo <khoa.jedi@gmail.com>
2021-12-25 11:11:56 +0700

    Update houses dataset

--- deep_learning/jedi/houses/dataset.py
+++ deep_learning/jedi/houses/dataset.py
@@ -2,6 +2,9 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import numpy as np
+import glob
+import cv2
+import os
 
 
 def load_attributes(inputPath):
@@ -34,3 +37,28 @@ def process_attributes(df, train, test):
     testX = np.hstack([testCategorical, testContinuous])
 
     return (trainX, testX)
+
+
+def load_house_images(df, inputPath):
+    images = []
+
+    for i in df.index.values:
+        basePath = os.path.sep.join([inputPath, "{}_*".format(i + 1)])
+        housePaths = sorted(list(glob.glob(basePath)))
+
+        inputImages = []
+        outputImage = np.zeros((64, 64, 3), dtype="uint8")
+
+        for housePath in housePaths:
+            image = cv2.imread(housePath)
+            image = cv2.resize(image, (32, 32))
+            inputImages.append(image)
+
+            outputImage[0:32, 0:32] = inputImages[0]
+            outputImage[0:32, 32:64] = inputImages[1]
+            outputImage[32:64, 32:64] = inputImages[2]
+            outputImage[32:64, 0:32] = inputImages[3]
+
+            images.append(outputImage)
+
+        return np.array(images)
