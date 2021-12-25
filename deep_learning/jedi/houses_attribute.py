2ed11d2e2b755faad0710dea64dee0c8f8f4ad5c
Khoa D. Vo <khoa.jedi@gmail.com>
2021-12-25 08:03:49 +0700

    Add houses attribute

---
+++ deep_learning/jedi/houses_attribute.py
@@ -0,0 +1,36 @@
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+
+
+def load_house_attributes(inputPath):
+    cols = ["bedrooms", "bathrooms", "area", "zipcode", "price"]
+    df = pd.read_csv(inputPath, sep=" ", header=None, names=cols)
+
+    zipcodes = df["zipcode"].value_counts().keys().tolist()
+    zipcounts = df["zipcode"].value_counts().tolist()
+
+    for (zipcode, count) in zip(zipcodes, zipcounts):
+        if count < 25:
+            idxs = df[df["zipcode"] == zipcode].index
+            df.drop(idxs, inplace=True)
+
+    return df
+
+
+def process_house_attributes(df, train, test):
+    continuous = ["bedrooms", "bathrooms", "area"]
+
+    cs = MinMaxScaler()
+    trainContinuous = cs.fit_transform(train[continuous])
+    testContinuous = cs.transform(test[continuous])
+
+    zipBinarizer = LabelBinarizer().fit(df["zipcode"])
+    trainCategorical = zipBinarizer.transform(train["zipcode"])
+    testCategorical = zipBinarizer.transform(test["zipcode"])
+
+    trainX = np.hstack([trainCategorical, trainContinuous])
+    testX = np.hstack([testCategorical, testContinuous])
+
+    return (trainX, testX)
